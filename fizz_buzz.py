6889e96bede9fcf0c0d4fcec78f53d3fb7b508e1
nnikolov3 <nnikolov@pdx.edu>
2019-08-31 12:23:00 -0700

    Our second python program

---
+++ fizz_buzz.py
@@ -0,0 +1,13 @@
+import sys # importing a library
+x = sys.argv[1] 
+print(sys.argv[0]) # this prints the name of the file
+print(x) # this print the command line input
+x = int(x)
+
+def
+    if (x % 3) / x  < 0 : # no need for brackets
+        print("Fizz")
+    if (x % 2 ) < 0:
+        print ("Buzz")
+    else : print ("Not mod 2 or 3")
+
