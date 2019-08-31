daf59164330d81fab2f5b36b53788b6555c52a2a
nnikolov3 <nnikolov@pdx.edu>
2019-08-31 13:05:51 -0700

    Adding classes

--- for.py
+++ for.py
@@ -1,11 +1,19 @@
 import sys
 
+class my_class:
+    def do_something(x):
+        print("did something")
+
 try: # it tries and if it does not exist goes to the "except"
     x = int(sys.argv[1])
 except:
     x = ['x','y', 'z','y']
     print("No Data")
 
-for y in enumerate (x[:-1]):
+for y in enumerate (x[:-1]): # all but the last one from the list
     print (str(y[1]))
+    my_class.do_something(y)
+
+    
+
 
